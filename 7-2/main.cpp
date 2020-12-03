7898f30330bff5a3e56644655e6d3125c3730827
miyautisaiki <asw1164@icloud.com>
2020-12-03 11:18:27 +0900

    プロジェクト ファイルを追加します。

---
+++ 7-2/main.cpp
@@ -0,0 +1,102 @@
+#include "DxLib.h"
+#include<time.h>
+
+const char TITLE[] = "GC1B_19_宮内彩輝";
+
+const int WIN_WIDTH = 600; //ウィンドウ横幅
+const int WIN_HEIGHT = 400;//ウィンドウ縦幅
+
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	ChangeWindowMode(TRUE);						//ウィンドウモードに設定
+	//ウィンドウサイズを手動では変更できず、かつウィンドウサイズに合わせて拡大できないようにする
+	SetWindowSizeChangeEnableFlag(FALSE, FALSE);
+	SetMainWindowText(TITLE);					// タイトルを変更
+	SetGraphMode(WIN_WIDTH, WIN_HEIGHT, 32);	//画面サイズの最大サイズ、カラービット数を設定（モニターの解像度に合わせる）
+	SetWindowSizeExtendRate(1.0);				//画面サイズを設定（解像度との比率で設定）
+	SetBackgroundColor(0xFF, 0xFF, 0xFF);		// 画面の背景色を設定する
+
+	//Dxライブラリの初期化
+	if (DxLib_Init() == -1) { return -1; }
+
+	//（ダブルバッファ）描画先グラフィック領域は裏面を指定
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	//画像などのリソースデータの変数宣言と読み込み
+
+
+
+	//ゲームループで使う変数の宣言
+	char keys[256] = { 0 }; //最新のキーボード情報用
+	char oldkeys[256] = { 0 };//1ループ（フレーム）前のキーボード情報
+
+	int ShakeTate = 100;
+	int ShakeYoko = 100;
+	int ObjTimer = 1500;
+	int ShakeWidth = 10;
+	srand(time(NULL));
+
+	// ゲームループ
+	while (1)
+	{
+		//最新のキーボード情報だったものは１フレーム前のキーボード情報として保存
+
+		//最新のキーボード情報を取得
+		GetHitKeyStateAll(keys);
+
+		//画面クリア
+		ClearDrawScreen();
+		//---------  ここからプログラムを記述  ----------//
+
+		//更新処理
+
+		if (ObjTimer > 0) {
+			ObjTimer -= 1;
+
+			ShakeTate = rand() % 10 * ShakeWidth;
+			ShakeYoko = rand() % 10 * ShakeWidth;
+
+			if (ShakeWidth > 0 && ObjTimer % 150 == 0) {
+				ShakeWidth -= 1;
+			}
+
+			if (rand() % 2 == FALSE) {
+				ShakeTate = ShakeTate * -1;
+			}
+
+			if (rand() % 2 == FALSE) {
+				ShakeYoko = ShakeYoko * -1;
+			}
+		}
+
+
+		//描画処理
+
+		for (int x = 0; x < 20; x++) {
+			DrawLine(30 * x + ShakeYoko, -400 + ShakeTate, 30 * x + ShakeYoko, 800 + ShakeTate, GetColor(0, 0, 0), TRUE);
+		}
+
+		for (int y = 0; y < 20; y++) {
+			DrawLine(-600 + ShakeYoko, y * 20 + ShakeTate, 1200 + ShakeYoko, y * 20 + ShakeTate, GetColor(0, 0, 0), TRUE);
+		}
+		//---------  ここまでにプログラムを記述  ---------//
+		ScreenFlip();//（ダブルバッファ）裏面
+		// 20ミリ秒待機（疑似60FPS）
+		WaitTimer(20);
+		// Windows システムからくる情報を処理する
+		if (ProcessMessage() == -1)
+		{
+			break;
+		}
+		// ＥＳＣキーが押されたらループから抜ける
+		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1)
+		{
+			break;
+		}
+	}
+	//Dxライブラリ終了処理
+	DxLib_End();
+
+	return 0;
+}
---
+++ 7-3/main.cpp
@@ -0,0 +1,123 @@
+#include "DxLib.h"
+
+const char TITLE[] = "学籍番号名前：タイトル";
+
+const int WIN_WIDTH = 1200; //ウィンドウ横幅
+const int WIN_HEIGHT = 400;//ウィンドウ縦幅
+
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	ChangeWindowMode(TRUE);						//ウィンドウモードに設定
+	//ウィンドウサイズを手動では変更できず、かつウィンドウサイズに合わせて拡大できないようにする
+	SetWindowSizeChangeEnableFlag(FALSE, FALSE);
+	SetMainWindowText(TITLE);					// タイトルを変更
+	SetGraphMode(WIN_WIDTH, WIN_HEIGHT, 32);	//画面サイズの最大サイズ、カラービット数を設定（モニターの解像度に合わせる）
+	SetWindowSizeExtendRate(1.0);				//画面サイズを設定（解像度との比率で設定）
+	SetBackgroundColor(0x00, 0x00, 0xFF);		// 画面の背景色を設定する
+
+	//Dxライブラリの初期化
+	if (DxLib_Init() == -1) { return -1; }
+
+	//（ダブルバッファ）描画先グラフィック領域は裏面を指定
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	//画像などのリソースデータの変数宣言と読み込み
+
+
+
+	//ゲームループで使う変数の宣言
+	char keys[256] = { 0 }; //最新のキーボード情報用
+	char oldkeys[256] = { 0 };//1ループ（フレーム）前のキーボード情報
+
+	int MouseX, MouseY;
+	double EasingX1[] = { 100.0 };
+	double EasingX2[] = { 100 };
+	double Vel1[] = { 50.0 };
+	double Vel2[] = { 1.0 };
+	int num = 0;
+
+	// ゲームループ
+	while (1)
+	{
+		//最新のキーボード情報だったものは１フレーム前のキーボード情報として保存
+
+		//最新のキーボード情報を取得
+		GetHitKeyStateAll(keys);
+
+		//画面クリア
+		ClearDrawScreen();
+		//---------  ここからプログラムを記述  ----------//
+
+		//更新処理
+
+
+		if (keys[KEY_INPUT_SPACE] == 1) {
+			num = 0;
+		}
+
+		num += 1;
+
+
+		SetDrawBlendMode(DX_BLENDMODE_ADD, 170);
+
+		DrawBox(100, 100, 116, 116, GetColor(255, 0, 0), TRUE);
+
+		for (int i = 0; i < num; i++) {
+
+			if (EasingX1[i] >= 900.0) {
+				EasingX1[i] = 900.0;
+				DrawBox(EasingX1[i], 100, EasingX1[i] + 16, 116, GetColor(255, 0, 0), TRUE);
+				break;
+			}
+
+			else if (i >0) {
+				Vel1[i] = Vel1[i - 1] * 1.1;
+				EasingX1[i] = Vel1[i] + 100;
+				DrawBox(EasingX1[i], 100, EasingX1[i] + 16, 116, GetColor(255, 255, 255), TRUE);
+			}
+		}
+
+		DrawBox(100, 200, 116, 216, GetColor(255, 0, 0), TRUE);
+
+		for (int i = 0; i < num; i++) {
+
+			if (EasingX2[i] >= 900.0) {
+				EasingX2[i] = 900.0;
+				DrawBox(EasingX2[i], 100, EasingX2[i] + 16, 116, GetColor(255, 0, 0), TRUE);
+				break;
+			}
+
+			else if (i >0) {
+				Vel2[i] = Vel2[i - 1] * 0.9;
+				EasingX2[i] = EasingX2[i - 1] + Vel2[i];
+				DrawBox(EasingX2[i], 200, EasingX2[i] + 16, 216, GetColor(255, 255, 255), TRUE);
+			}
+		}
+
+
+		//描画処理
+
+
+
+
+		//---------  ここまでにプログラムを記述  ---------//
+		ScreenFlip();//（ダブルバッファ）裏面
+		// 20ミリ秒待機（疑似60FPS）
+		WaitTimer(20);
+		// Windows システムからくる情報を処理する
+		if (ProcessMessage() == -1)
+		{
+			break;
+		}
+		// ＥＳＣキーが押されたらループから抜ける
+		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1)
+		{
+			break;
+		}
+	}
+	//Dxライブラリ終了処理
+	DxLib_End();
+
+	return 0;
+}
